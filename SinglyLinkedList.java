aed65bb5ef352c723f2d30e40f8857bf6f3b287c
Luis Santiago <luissantiago@Luiss-Air.lan>
2024-02-13 12:56:58 -0500

    search for node , delete node , delete linked list

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -58,32 +58,102 @@ public void insertInLinkedList(int nodeValue,int location){
  
 public void traverseSinglyLinkedList(){
     if (head ==null){
-        System.out.println("Singly linked list does not exist");
+        System.out.println("Singly linked list does not exist");   // 0(1)
     }
     else {
-        Node tempNode =head;
-        for (int i =0; i <size;i++){
-            System.out.println(tempNode.value);
-            if (i != size-1){
+        Node tempNode =head;//0(1)
+        for (int i =0; i <size;i++){ // 0(N)
+            System.out.print(tempNode.value);
+            if (i != size-1){ // 0(1)
                 System.out.print("->");
             }
-            tempNode = tempNode.next;
+            tempNode = tempNode.next;//0(1)
         }
     }
-    System.out.println();
+    System.out.println();// 0(1)
+    }// time complexity 0(N) space complexity 0 (1)
+
+
+    public boolean searchNode(int nodeValue){
+        if (head != null){//0(1)
+            Node tempNode = head;//0(1)
+            for (int i=0 ; i <size;i++){ // 0(N)
+                if (tempNode.value == nodeValue){
+                    System.out.println("Found node at location "+i );//0(1)
+                    return true;
+                }
+                tempNode = tempNode.next;
+            }
+        }
+
+System.out.println("Node not found");
+return false;
+
+    }// tIME COMPLEXITY 0(N) SPACE COMPLEXITY 0(1)
+
+
+public void deleteNode (int location){
+if (head ==null){
+    System.out.println("The SLL does not exist");
+    return;
+}
+else if (location ==0){
+    head = head.next;
+    size --;
+    if (size ==0){
+        tail = null;
+    }
+}
+else if (location >= size){
+        Node tempNode = head;
+        for (int i =0;i< size-1 ;i++){
+            tempNode = tempNode.next;
+        }
+        if (tempNode == head){
+            tail = head = null;
+            size --;
+            return ; 
+        }
+        tempNode.next = null;
+        tail = tempNode;
+        size--;
+    }
+else {
+        Node tempNode = head;
+        for (int i=0 ; i<location-1;i++){
+            tempNode = tempNode.next;
+        }
+        tempNode.next = tempNode.next.next;
+        size--;
     }
 
-public static void main(String[] args) {
+}
+
+
+public void deleteSinglyLinkedList(){
+head = null;
+tail = null;
+System.out.println("The SLL has been deleted");
+}
+
+
+
+
+    public static void main(String[] args) {
     SinglyLinkedList sLL = new SinglyLinkedList();
     sLL.createSinglyLinkedList(1);
 
    // System.out.println(sLL.head.value);
    sLL.insertInLinkedList(6,1);
+   sLL.insertInLinkedList(5,2);
    sLL.insertInLinkedList(7,3);
    sLL.insertInLinkedList(8,4);
    sLL.insertInLinkedList(9,5);
    sLL.traverseSinglyLinkedList();
-
+   //sLL.searchNode(6);
+sLL.deleteNode(5);
+sLL.traverseSinglyLinkedList();
+sLL.deleteSinglyLinkedList();
 
 }
 
